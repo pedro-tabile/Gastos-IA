9323c6a5538efe08faa745cbec33f93681d69c6b
Pedro364898 <tabilepedro1903@gmail.com>
2026-08-17 19:02:38 -0300

    Commit inicial

---
+++ src/test/java/backend/spring_ai/SpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.spring_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
