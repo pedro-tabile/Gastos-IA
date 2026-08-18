6bbf4a285f2aca17b92f4ac5f994b948cb559e14
Pedro364898 <tabilepedro1903@gmail.com>
2026-08-17 23:39:20 -0300

    Implementação de controller e teste para Chat Client

--- src/main/java/backend/spring_ai/SpringAiApplication.java
+++ src/main/java/backend/spring_ai/SpringAiApplication.java
@@ -1,10 +1,17 @@
 package backend.spring_ai;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringAiApplication {
+
+    @Bean
+    ChatClient chatClient(ChatClient.Builder chatClient) {
+        return chatClient.build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringAiApplication.class, args);
