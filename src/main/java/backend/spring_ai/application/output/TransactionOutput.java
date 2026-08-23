015caafdd45cfc0bd4f0c298629b14a7160ee114
Pedro364898 <tabilepedro1903@gmail.com>
2026-08-23 00:38:01 -0300

    Implementação de campo createdAt (LocalDateTime) no banco de dados

--- src/main/java/backend/spring_ai/application/output/TransactionOutput.java
+++ src/main/java/backend/spring_ai/application/output/TransactionOutput.java
@@ -5,14 +5,16 @@ import backend.spring_ai.domain.Transaction;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.LocalDateTime;
 
-public record TransactionOutput(String id, String description, double value, Category category) {
+public record TransactionOutput(String id, String description, double value, Category category, LocalDateTime createdAt) {
     public static TransactionOutput from(Transaction transaction) {
         return new TransactionOutput(
                 transaction.getId().id().toString(),
                 transaction.getDescription(),
                 BigDecimal.valueOf(transaction.getAmount()).setScale(2, RoundingMode.HALF_EVEN).doubleValue(),
-                transaction.getCategory()
+                transaction.getCategory(),
+                transaction.getCreatedAt()
         );
     }
 }
--- src/main/java/backend/spring_ai/infrastructure/http/dto/response/TransactionResponse.java
+++ src/main/java/backend/spring_ai/infrastructure/http/dto/response/TransactionResponse.java
@@ -3,8 +3,10 @@ package backend.spring_ai.infrastructure.http.dto.response;
 import backend.spring_ai.application.output.TransactionOutput;
 import backend.spring_ai.domain.Category;
 
-public record TransactionResponse(String id, String description, double amount, Category category) {
+import java.time.LocalDateTime;
+
+public record TransactionResponse(String id, String description, double amount, Category category, LocalDateTime createdAt) {
     public static TransactionResponse from(TransactionOutput output) {
-        return new TransactionResponse(output.id(), output.description(), output.value(), output.category());
+        return new TransactionResponse(output.id(), output.description(), output.value(), output.category(), output.createdAt());
     }
 }
