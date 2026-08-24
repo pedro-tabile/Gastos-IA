37a29610e308c3a08cdd25fa8f485b2833702e51
Pedro364898 <tabilepedro1903@gmail.com>
2026-08-23 23:46:26 -0300

    Ajustes e readme

--- src/main/java/backend/spring_ai/IAs/TextToSpeechController.java
+++ src/main/java/backend/spring_ai/IAs/TextToSpeechController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,7 +23,7 @@ public class TextToSpeechController {
     }
 
     @PostMapping(value = "/synthesize", produces = "audio/mp3")
-    public ResponseEntity<Resource> transcribe(@RequestBody SynthesizeRequest request) {
+    public ResponseEntity<Resource> transcribe(@RequestParam SynthesizeRequest request) {
         byte[] audio = textToSpeechModel.call(request.text());
         var resource = new ByteArrayResource(audio);
 
